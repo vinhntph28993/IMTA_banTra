a7f931de139ef67dc482679c30f26fd8d166639f
vinhntph28993 <tienvinh21022003@gmail.com>
2024-06-25 23:18:58 +0700

    aaaaaaa

---
+++ WebsiteBanTraiCay/TetsttttGIt/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteBanTraiCay.TetsttttGIt
+{
+    internal interface Interface1
+    {
+    }
+}
